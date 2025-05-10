26238ff65cf03badc4656dac6ea146fa539410c5
18Nithya07 <18nithya07@gmail.com>
2025-05-10 14:18:20 +0530

    Add files via upload

---
+++ demo/HTML 2/Dev3/Day 4/exp 1.js
@@ -0,0 +1,31 @@
+
+//let n = document.getElementById("Nithya")
+//n.innerHTML = "Nithya"
+//let b= document.getElementById("R24EF197")
+//b.innerHTML = "463721h"
+
+
+//let c = document.getElementById("Reva")
+//c.innerHTML = "Reva"
+
+function data(){
+    let a = document.getElementById("first name")
+let data = a.value
+let display = document.getElementById("display1")
+display.innerHTML = data
+
+}
+function datas(){
+    let b = document.getElementById("second name")
+let datas = b.value
+let display = document.getElementById("display2")
+display.innerHTML = datas
+
+}
+function ni(){
+    let c = document.getElementById("third name")
+    let ni = c.value
+    let display = document.getElementById("display3")
+    display.innerHTML = ni
+
+}
